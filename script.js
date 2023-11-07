bf7b4fa028d7701e462acafc7beb332cda131c51
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-07 16:48:25 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+// Find the element with the id "level"
+const targetElement = document.getElementById("level");
+
+// Initialize a variable to keep track of the DOM level
+let domLevel = 0;
+
+// Traverse the parent elements until we reach the root "html" element
+let currentElement = targetElement;
+while (currentElement && currentElement !== document.documentElement) {
+    domLevel++;
+    currentElement = currentElement.parentElement;
+}
+
+// Display the result using the alert function
+alert(`The level of the element is: ${domLevel}`);
